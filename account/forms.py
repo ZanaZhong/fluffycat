887e68b16edff070abd19b8c87a873e3e67f9082
Zana <zana@zhongjiarongde-MacBook-Pro.local>
2019-11-27 23:20:09 +0800

    modify register.html

--- account/forms.py
+++ account/forms.py
@@ -11,7 +11,7 @@ class RegisterForm(forms.Form):
         ('male','男'),
         ('female','女'),
     )
-    username = forms.CharField(label="用戶名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label="姓名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label="密碼", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     checkpassword = forms.CharField(label="確認密碼", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     address = forms.CharField(label="地址", max_length=150, widget=forms.TextInput(attrs={'class': 'form-control'}))
