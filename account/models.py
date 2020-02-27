1f89467089d693e8d80b9c8e3cd27390f51dae60
ZanaZhong <108423021@ncu.edu.tw>
2020-02-27 10:38:59 +0800

    modify account model

--- account/models.py
+++ account/models.py
@@ -17,25 +17,11 @@ class Account(models.Model):
     ctime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return str(self.id)
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Account._meta.fields]
 # admin.site.register(Accounts,AccountAdmin)
 
-class Violate(models.Model):
-    vlist = (
-        ('圖文不符','圖文不符'),
-        ('洗版','洗版'),
-        ('其他','其他'),
-    )
-    name = models.CharField('名字', max_length=32)
-    violateList = models.CharField(max_length=32,choices=vlist,default="圖文不符")
 
-    def __str__(self):
-        return self.id
-
-@admin.register(Violate)
-class ViolateAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Violate._meta.fields]
